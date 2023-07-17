691399a1e4c06da6deae89ed2fce451483028eeb
Matteaz <79192782+gabrielematteaz@users.noreply.github.com>
2023-07-17 16:29:16 +0200

    new version

--- catbin/catbin.c
+++ catbin/catbin.c
@@ -1,5 +1,5 @@
-#include "../mttlib/mttopt/mttopt.h"
 #include "../mttlib/mttfile/mttfile.h"
+#include "../mttlib/mttopt/mttopt.h"
 #include "../mttlib/mttstr/mttstr.h"
 
 int main(int argc, char *argv[])
@@ -8,24 +8,21 @@ int main(int argc, char *argv[])
 		{ 'o', 0, MUST_HAVE_ARG, 0 },
 		{ 'l', 0, MUST_HAVE_ARG, 0 },
 		{ 's', 0, MUST_HAVE_ARG, 0 },
-		{ 'c', 0, MUST_HAVE_ARG, 0 },
+		{ 'c', 0, MUST_HAVE_ARG, 0 }
 	};
 
-	int avoff;
-	char *cont, *line, *lhs, *lcs, *c, *cs;
-	size_t off, lim, size, sects, cols, bytesxrow, rowcount, rownummaxwidth, hexsectoff, charsectoff, linesize, sect, col, rownum;
+	int avoff = mttopt_extract_optv(argc, argv, sizeof(optv) / sizeof(*optv), optv);
+	struct mttstr_fmt_t optfmt = { 0, '-', ' ', 10, 0 }, rowfmt, hexfmt;
+	long off = mttstr_fstr_to_ival(optv[0].arg, NULL, optfmt);
+	char *cont, *line, *l, *lhexsect, *lcharsect, *lhs, *lcs, *c, *cs;
+	size_t lim = optv[1].status == FOUND ? mttstr_fstr_to_ival(optv[1].arg, NULL, optfmt) : 256, size = mttfile_load_file(argv[avoff], &cont, off, &lim, RESIZE), sects, cols, bytesxrow, rowcount, linesize, sect, col, row;
 	unsigned char cc;
-	
-	avoff = mttopt_extract_optv(argc, argv, sizeof(optv) / sizeof(*optv), optv);
-	off = optv[0].status == FOUND ? mttstr_fstr_to_ival(optv[0].arg, NULL, 10, '+', '-', ' ', LEFT, 0) : 0;
-	lim = optv[1].status == FOUND ? mttstr_fstr_to_ival(optv[1].arg, NULL, 10, '+', '-', ' ', LEFT, 0) : 256;
-	size = mttfile_load_file(argv[avoff], &cont, off, &lim, RESIZE);
 
 	if (size)
 	{
 		if (optv[2].status == FOUND)
 		{
-			sects = mttstr_fstr_to_ival(optv[2].arg, NULL, 10, '+', '-', ' ', LEFT, 0);
+			sects = mttstr_fstr_to_ival(optv[2].arg, NULL, optfmt);
 
 			if (sects == 0) sects = 2;
 		}
@@ -33,9 +30,9 @@ int main(int argc, char *argv[])
 
 		if (optv[3].status == FOUND)
 		{
-			cols = mttstr_fstr_to_ival(optv[3].arg, NULL, 10, '+', '-', ' ', LEFT, 0);
+			cols = mttstr_fstr_to_ival(optv[3].arg, NULL, optfmt);
 
-			if (cols == 0) sects = 8;
+			if (cols == 0) cols = 8;
 		}
 		else cols = 8;
 
@@ -44,72 +41,108 @@ int main(int argc, char *argv[])
 
 		if (size % bytesxrow) rowcount++;
 
-		rownummaxwidth = mttstr_ival_to_fstr(NULL, rowcount, 16, 0, 0, 0, 0, 0, 0);
-		hexsectoff = rownummaxwidth;
-		charsectoff = 3 + hexsectoff + sects * (2 * cols + cols - 1) + 2 * (sects - 1);
-		linesize = charsectoff + 3 + sects * cols + sects;
+		rowfmt.plus = 0;
+		rowfmt.minus = 0;
+		rowfmt.fill = '0';
+		rowfmt.base = 16;
+		rowfmt.fillmode = LEFT;
+		rowfmt.flags = 0;
+		rowfmt.width = 0;
+		rowfmt.width = mttstr_ival_to_fstr(NULL, rowcount, rowfmt);
+		linesize = rowfmt.width + 3 + sects * (2 * cols + cols - 1) + 2 * (sects - 1) + 3 + sects * cols + sects;
 		line = malloc(linesize);
+
 		if (line != NULL)
 		{
-			lhs = line + hexsectoff;
-			lcs = line + charsectoff;
-			*lhs = ' ';
-			lhs++;
-			*lhs = ' ';
-			lhs++;
-			*lhs = ' ';
-			lhs++;
-			*lcs = ' ';
-			lcs++;
-			*lcs = ' ';
-			lcs++;
-			*lcs = ' ';
-			lcs++;
+			l = line + rowfmt.width;
+			*l = ' ';
+			l++;
+			*l = ' ';
+			l++;
+			*l = ' ';
+			l++;
+			lhexsect = l;
 			sect = 0;
+
 			while (1)
 			{
+				sect++;
 				col = 0;
+
 				while (1)
 				{
-					lhs = lhs + 2;
+					l = l + 2;
 					col++;
+
 					if (col == cols) break;
-					*lhs = ' ';
-					lhs++;
+
+					*l = ' ';
+					l++;
 				}
-				lcs = lcs + cols;
-				sect++;
+
 				if (sect == sects) break;
-				*lhs = ' ';
-				lhs++;
-				*lhs = ' ';
-				lhs++;
-				*lcs = ' ';
-				lcs++;
+
+				*l = ' ';
+				l++;
+				*l = ' ';
+				l++;
 			}
-			*lcs = 0;
+
+			*l = ' ';
+			l++;
+			*l = ' ';
+			l++;
+			*l = ' ';
+			l++;
+			lcharsect = l;
+			sect = 0;
+
+			while (1)
+			{
+				l = l + cols;
+				sect++;
+
+				if (sect == sects) break;
+
+				*l = ' ';
+				l++;
+			}
+
+			hexfmt.plus = 0;
+			hexfmt.minus = 0;
+			hexfmt.fill = '0';
+			hexfmt.base = 16;
+			hexfmt.fillmode = LEFT;
+			hexfmt.flags = 0;
+			hexfmt.width = 2;
+			*l = 0;
 			c = cont;
 			cs = c + size;
-			rownum = 0;
+			row = 0;
+
 			while (1)
 			{
-				mttstr_ival_to_fstr(line, rownum, 16, 0, 0, '0', LEFT, rownummaxwidth, 0);
-				lhs = line + hexsectoff + 3;
-				lcs = line + charsectoff + 3;
+				mttstr_ival_to_fstr(line, row, hexfmt);
+				lhs = lhexsect;
+				lcs = lcharsect;
 				sect = 0;
+				row++;
+
 				while (1)
 				{
-					col = 0;
+					sect++;
+					col = 1;
+
 					while (1)
 					{
 						cc = *c;
-						lhs = lhs + mttstr_ival_to_fstr(lhs, cc, 16, 0, 0, '0', LEFT, 2, 0);
+						lhs = lhs + mttstr_ival_to_fstr(lhs, cc, hexfmt);
 						*lcs = 32 <= cc && cc <= 126 ? cc : '.';
 						lcs++;
 						c++;
+
 						if (c == cs)
 						{
-							col++;
 							while (1)
 							{
 								while (col < cols)
@@ -124,34 +157,43 @@ int main(int argc, char *argv[])
 									lcs++;
 									col++;
 								}
-								sect++;
+
 								if (sect == sects) break;
+
 								lhs = lhs + 2;
 								lcs++;
+								sect++;
 								col = 0;
+
 								goto next;
 							}
+
 							puts(line);
+
 							goto exit;
 						}
-						col++;
+
 						if (col == cols) break;
+
 						lhs++;
+						col++;
 					}
-					sect++;
+
 					if (sect == sects) break;
+
 					lhs = lhs + 2;
 					lcs++;
 				}
+
 				puts(line);
-				rownum++;
 			}
+
 		exit:
 			free(line);
 		}
+
 		free(cont);
 	}
-
 
 	return 0;
 }
