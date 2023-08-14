bcbed6cfedfde945113ac7674c9a5455ce2fa3b7
Matteaz <79192782+gabrielematteaz@users.noreply.github.com>
2023-08-14 11:49:07 +0200

    new version

---
+++ mttlib/mttopt/mttopt.hpp
@@ -0,0 +1,28 @@
+#ifndef MTTOPT_HPP
+#define MTTOPT_HPP
+
+#include <vector>
+
+struct mttopt_opt_t
+{
+	uint8_t shrt;
+	uint8_t flags;
+	bool found;
+	const char *arg;
+
+	mttopt_opt_t() noexcept;
+	mttopt_opt_t(uint8_t shrt, uint8_t flags) noexcept;
+};
+
+#define OPT_FLAGS_OVERWRITE_ON_COPY 0
+#define OPT_FLAGS_IGNORE_COPIES 1
+#define OPT_FLAGS_EXIT_ON_COPY 2
+#define OPT_FLAGS_HAS_NO_ARG 0
+#define OPT_FLAGS_CAN_HAVE_ARG 4
+#define OPT_FLAGS_MUST_HAVE_ARG 12
+
+using mttopt_optv_t = std::vector < mttopt_opt_t >;
+
+int mttopt_extr_optv(int argc, char *argv[], mttopt_optv_t &optv) noexcept;
+
+#endif
