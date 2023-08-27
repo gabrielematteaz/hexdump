cc24248ce07725b138e8a8731c46dd45758d255e
Matteaz <79192782+gabrielematteaz@users.noreply.github.com>
2023-08-27 14:26:25 +0200

    old

--- mttlib/mttfile/mttfile.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MTTFILE_HPP
-#define MTTFILE_HPP
-
-#include <vector>
-#include <fstream>
-
-std::vector < char > mttfile_load_file(const char *name, std::streampos off, std::streamsize lim, bool str);
-
-#endif
